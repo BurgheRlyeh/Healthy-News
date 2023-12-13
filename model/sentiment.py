3fa676e8a791b9ddcf8b2ae511cb2e737bd66a3d
BurgheRlyeh <egor6tlou@gmail.com>
2023-12-13 15:57:23 +0300

    config

--- model/sentiment.py
+++ model/sentiment.py
@@ -14,12 +14,6 @@ df['review'] = df['review'].apply(lambda x: re.sub(r'[^\w\s]', '', x))
 df['review'] = df['review'].str.lower()
 
 def make_docs(data):
-    """
-    this will take a list of texts and labels
-    and transform them in spacy documents
-    data: list(tuple(text, label))
-    returns: List(spacy.Doc.doc)
-    """
     docs = []
     a = tqdm(nlp.pipe(data, as_tuples=True), total = len(data))
     for doc, label in tqdm(nlp.pipe(data, as_tuples=True), total = len(data)):
